62eac2efbe1de869b41db314f33844c33d189fa7
Shano Domingo <domingoshan17@gmail.com>
2026-06-24 09:22:40 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.VAT_DASHBOARD_SUPABASE_CONFIG={
-  url:'https://pqxspjlmxltxpnhvivdy.supabase.co',
-  publishableKey:'sb_publishable_LtjYerjDYfGusEa8tOGvUg_3VcDJfh4',
-  table:'ShanoooDomingoAFK'
-};
